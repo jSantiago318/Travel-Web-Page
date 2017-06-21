55c39715c5822a6a793d1db25d5f241fc865e021
Joshua Santiago <joshuasantiago111@gmail.com>
2017-06-21 09:13:49 -0400

    update1.1

--- public/javascripts/animation.js
+++ public/javascripts/animation.js
@@ -1,39 +1,37 @@
+/////////////////////////////////////////////////////
 var options = [{
-    selector: '#staggered-test',
-    offset: 50,
-    callback: function(el) {
-      Materialize.toast("This is our ScrollFire Demo!", 1500);
-    }
-  },
-  {
-    selector: '#staggered-test',
-    offset: 205,
-    callback: function(el) {
-      Materialize.toast("Please continue scrolling!", 1500);
-    }
-  },
-  {
-    selector: '#staggered-test',
-    offset: 400,
-    callback: function(el) {
-      Materialize.showStaggeredList($(el));
-    }
-  },
-  {
-    selector: '#image-test',
-    offset: 500,
-    callback: function(el) {
-      Materialize.fadeInImage($(el));
-    }
+  selector: '#staggered-test'
+  , offset: 50
+  , callback: function(el) {
+    Materialize.toast("This is our ScrollFire Demo!", 1500);
   }
-];
+}, {
+  selector: '#staggered-test'
+  , offset: 205
+  , callback: function(el) {
+    Materialize.toast("Please continue scrolling!", 1500);
+  }
+}, {
+  selector: '#staggered-test'
+  , offset: 400
+  , callback: function(el) {
+    Materialize.showStaggeredList($(el));
+  }
+}, {
+  selector: '#image-test'
+  , offset: 500
+  , callback: function(el) {
+    Materialize.fadeInImage($(el));
+  }
+}];
 Materialize.scrollFire(options);
 
 
+////////////////////////////////////////////////
+var vid = document.getElementById("bgvid");
 
 
-
-
+////////////////////////////////////////////////
 $('.button-collapse').sideNav({
   menuWidth: 300, // Default is 300
   edge: 'right', // Choose the horizontal origin
@@ -42,11 +40,6 @@ $('.button-collapse').sideNav({
 });
 
 
-
-
-
-
-
-
+////////////////////////////////////////////////
 $('.dropdown-button').dropdown('open');
 $('dropdown-button').dropdown('close');
